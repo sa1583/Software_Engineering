ce8fbc69b1957c375d6c432e15ede262d029b926
2014004111 <kares64@gmail.com>
2018-12-03 20:27:32 +0900

    some change in Customer

--- market-client/src/classes/TradeInfo.js
+++ market-client/src/classes/TradeInfo.js
@@ -6,4 +6,5 @@ function TradeInfo(buyerid, tradedateinfo, products, tradeid, deltamileage, isus
   this.deltaMileage = deltamileage
   this.isUseCard = isusecard
   this.totalPrice = totalprice
+
 };
