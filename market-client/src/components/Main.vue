9e83132fbee1fefb702e507a138344c61f7cac35
채상욱 <2014004157@hanyang.ac.kr>
2018-12-03 21:48:21 +0900

    good

--- market-client/src/components/Main.vue
+++ market-client/src/components/Main.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <container style="width:60%; float:left">
-      <button>구매</button>
-      <button>환불</button><br>
-      <button>마일리지 확인</button><br>
-      <button>관리자 로그인</button><br>
-      <button>회원가입</button><br>
+      <button class="short" v-on:click="movePage('buy')">구매</button>
+      <button class="short" v-on:click="movePage('refund')">환불</button><br>
+      <button class="long" v-on:click="movePage('mileage')">마일리지 확인</button><br>
+      <button class="long" v-on:click="movePage('signup')">회원가입</button><br>
+      <button class="long" v-on:click="movePage('login')">관리자 로그인</button><br>
     </container>
     <container style="width:40%; float:right">
       <table>
@@ -23,12 +23,27 @@
   </div>
 </template>
 <script>
+import router from '../router'
 export default {
   name: 'Main',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  methods:{
+    movePage(page){
+      router.push({name:page})
+    }
   }
 }
 </script>
+
+<style>
+  button.short{
+    width:45%;
+  }
+  button.long{
+    width:90.5%;
+  }
+</style>
--- market-client/src/router/index.js
+++ market-client/src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/Main'
+import Buy from '@/components/Buy'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
-    path: '/',
-    name: 'home',
-    component: Main
-  }]
+      path: '/',
+      name: 'home',
+      component: Main
+    },
+    {
+      path: '/buy',
+      name: 'buy',
+      component: Buy
+    }
+  ]
 })
