9de556fb0ba15bfdcb885d4bdc3c26d29aed19a5
2014004111 <kares64@gmail.com>
2018-12-05 22:03:08 +0900

    LYC part complete

--- market-client/src/router/index.js
+++ market-client/src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/Main'
+import AdminPage from '@/components/AdminPage'
+import Payment from '@/components/Payment'
+import InputMemNum from '@/components/InputMemNum'
+import Refund from '@/components/Refund'
+import InputReceipt from '@/components/InputReceipt'
+
 
 Vue.use(Router)
 
@@ -8,6 +14,31 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'home',
-    component: Main
+    component: Main   //Main
+  },
+  {
+    path: '/AdminPage',
+    name: 'home',
+    component: AdminPage
+  },
+  {
+    path: '/InputMemNum',
+    name: 'home',
+    component: InputMemNum
+  },
+  {
+    path: '/Payment',
+    name: 'home',
+    component: Payment
+  },
+  {
+    path: '/Refund',
+    name: 'home',
+    component: Refund
+  },
+  {
+    path: '/InputReceipt',
+    name: 'home',
+    component: InputReceipt
   }]
 })
