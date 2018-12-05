8fcb19613fb32c214ff0469e0c1516ca4283fe2d
김병찬 <kbcbang@gmail.com>
2018-12-06 00:51:06 +0900

    back

--- market-client/src/classes/TradeInfo.js
+++
@@ -1,9 +0,0 @@
-function TradeInfo(buyerid, tradedateinfo, products, tradeid, deltamileage, isusecard, totalprice) = {
-  this.buyerID = buyerid
-  this.tradeDatetime = tradedateinfo
-  this.products = products
-  this.TradeID = tradeid
-  this.deltaMileage = deltamileage
-  this.isUseCard = isusecard
-  this.totalPrice = totalprice
-};
